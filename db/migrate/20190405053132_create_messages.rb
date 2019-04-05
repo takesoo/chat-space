93a56138c19121524dc019d06b2cb17e92a5f083
takesoo <htk.programming@gmail.com>
2019-04-05 15:14:11 +0900

    deviseを実装した
    deviseGemのインストール
    userモデルの作成
    userマイグレーションファイルの編集
    groupマイグレーションファイルの編集
    messageマイグレーションファイルの編集
    usersテーブルの作成
    rake db:migrate

--- db/migrate/20190405053132_create_messages.rb
+++ db/migrate/20190405053132_create_messages.rb
@@ -1,7 +1,10 @@
 class CreateMessages < ActiveRecord::Migration[5.0]
   def change
     create_table :messages do |t|
-
+      t.text :body, null: true
+      t.string :image, null: true
+      t.integer :user_id, null: false, foreign_key: true
+      t.integer :group_id, null: false, foreign_key: true
       t.timestamps
     end
   end
