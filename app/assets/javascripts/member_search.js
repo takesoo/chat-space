5b07cc30f8d5db0a7bec591685085d1f48ce85b9
takesoo <htk.programming@gmail.com>
2019-04-30 16:22:36 +0900

    インクリメンタルサーチ実装

--- app/assets/javascripts/member_search.js
+++ app/assets/javascripts/member_search.js
@@ -1,5 +1,18 @@
 $(function () {
-  $(".chat-group-form__input").on("keyup", function () {
+  function buildResultHTML(user) {
+    var html = `<div class="chat-group-user clearfix">
+                  <p class="chat-group-user__name">${user.name}</p>
+                  <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
+                </div>
+              `
+    $("#user-search-result").append(html);
+  }
+
+  function resultReset() {
+    $("#user-search-result").empty();
+  }
+
+  $("#user-search-field").on('keyup', function () {
     var input = $(this).val();
     $.ajax({
       type: 'GET',
@@ -8,10 +21,17 @@ $(function () {
       dataType: 'json'
     })
     .done(function (users) {
-      console.log(users);
+      resultReset()
+      if (users.length !== 0 && input.length !== 0) {
+        users.forEach(function (user) {
+          buildResultHTML(user)
+        });
+      } else {
+        resultReset()
+      }
     })
     .fail(function () {
-      alert("error");
+      alert("ユーザー検索に失敗しました");
     })
   });
 })
