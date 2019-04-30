4178baeef2a880bac5ee31750eb22242f533cc4c
takesoo <htk.programming@gmail.com>
2019-04-30 13:50:25 +0900

    json取得

--- app/assets/javascripts/member_search.js
+++ app/assets/javascripts/member_search.js
@@ -1,5 +1,17 @@
 $(function () {
   $(".chat-group-form__input").on("keyup", function () {
-    console.log("できてるよ");
+    var input = $(this).val();
+    $.ajax({
+      type: 'GET',
+      url: '/users',
+      data: { keyword: input },
+      dataType: 'json'
+    })
+    .done(function (users) {
+      console.log(users);
+    })
+    .fail(function () {
+      alert("error");
+    })
   });
 })
