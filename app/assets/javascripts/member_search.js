eb51e824542dd1ca28e195304c380e5911a5ecb2
takesoo <htk.programming@gmail.com>
2019-04-30 19:05:56 +0900

    追加ボタン機能実装

--- app/assets/javascripts/member_search.js
+++ app/assets/javascripts/member_search.js
@@ -1,15 +1,23 @@
 $(function () {
   function buildResultHTML(user) {
-    var html = `<div class="chat-group-user clearfix">
+    var searchedHTML = `<div class="chat-group-user clearfix">
                   <p class="chat-group-user__name">${user.name}</p>
                   <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
-                </div>
-              `
-    $("#user-search-result").append(html);
+                </div>`
+    $("#user-search-result").append(searchedHTML);
   }
 
   function resultReset() {
     $("#user-search-result").empty();
+  }
+
+  function buildAddHTML(user_data) {
+    var addMemberHTML = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-${user_data.userId}'>
+                          <input name='group[user_ids][]' type='hidden' value='${user_data.userId}'>
+                          <p class='chat-group-user__name'>${user_data.userName}</p>
+                          <div class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</div>
+                        </div>`
+    $("#chat-group-users").append(addMemberHTML);
   }
 
   $("#user-search-field").on('keyup', function () {
@@ -36,6 +44,9 @@ $(function () {
   });
 
   $(document).on('click', '.chat-group-user__btn--add', function () {
-    console.log("ボタンクリック")
+    var user_data = $(this).data()
+    $(this).parent().remove()
+    buildAddHTML(user_data)
   });
+
 })
