4da0d8e4748206af88223eebe0762127490dcbcb
takesoo <htk.programming@gmail.com>
2019-05-20 22:02:20 +0900

    basic認証実装

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,8 @@ server "54.92.102.160", user: "ec2-user", roles: %w{app db web}
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-
+set :rails_env, "production"
+set :unicorn_rack_env, "production"
 
 # role-based syntax
 # ==================
